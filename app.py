d0ea281fcc5f8b89cd44caa69e77080ce0fd1d19
Nkosinathi Nkosi <Nkosinathiduncan3@gmail.com>
2026-07-26 11:48:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ def sitemap_xml():
     content = f"""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://burgersfortwaterdrilling.co.za/</loc>
+    <loc>https://bgfwaterdrilling.co.za/</loc>
     <lastmod>{today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
